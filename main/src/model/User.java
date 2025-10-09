a1c501d8451ddbe34bde18a69630f2390fefdb49
paandonov22 <PAAndonov22@codingburgas.bg>
2025-10-09 11:06:44 +0300

    Organize files in folders. Add User.java

---
+++ main/src/model/User.java
@@ -0,0 +1,21 @@
+package model;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getEmail() { return email; }
+}
